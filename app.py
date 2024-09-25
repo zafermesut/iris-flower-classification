7db16131ff443675126478b35b84c90b9f8433c9
Zafer Mesut Bilen <wildones.zafer@gmail.com>
2024-09-25 15:52:26 +0300

    wip

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from joblib import load
+
+
+model = load('iris_model.joblib')
+
+def predict_species(sepal_length, sepal_width, petal_length, petal_width):
+    features = np.array([sepal_length, sepal_width, petal_length, petal_width]).reshape(1, -1)
+    prediction = model.predict(features)
+    prediction = prediction[0]
+    return prediction
+
+
+st.title('Iris Flower Prediction App')
+
+sepal_length = st.number_input('Sepal Length', min_value=0.0, max_value=10.0, value=5.0, step=0.1)
+sepal_width = st.number_input('Sepal Width', min_value=0.0, max_value=10.0, value=3.0, step=0.1)
+petal_length = st.number_input('Petal Length', min_value=0.0, max_value=10.0, value=1.0, step=0.1)
+petal_width = st.number_input('Petal Width', min_value=0.0, max_value=10.0, value=0.1, step=0.1)
+
+if st.button('Predict'):
+    prediction = predict_species(sepal_length, sepal_width, petal_length, petal_width)
+    st.write(f'The predicted species is {prediction}')
+
+
